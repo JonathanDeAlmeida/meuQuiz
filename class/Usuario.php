52b7d0144fa5945829fd64635724a849288f71ca
Everson2626 <48109366+Everson2626@users.noreply.github.com>
2020-10-05 21:54:19 -0300

    Add files via upload

---
+++ class/Usuario.php
@@ -0,0 +1,16 @@
+<?php
+
+
+abstract class Usuario{
+    var $nome;
+
+    public function __construct($nome){
+        $this->nome = $nome;
+    }
+    public function pesquisar($texto){
+
+    }
+    public function jogar(){
+
+    }
+}
